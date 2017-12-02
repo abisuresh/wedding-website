3f313b374088f8745721aa13a8c07ea6c656b0ec
Abi Suresh <aesther2012@gmail.com>
2017-12-01 21:50:55 -0500

    Final Draft with responsiveness :)

--- js/plugins.js
+++ js/plugins.js
@@ -22,6 +22,7 @@
     }
 }());
 
+//Closing dropdown menu on touch elsewhere on a touchscreen device
 
 $(document).on('touchstart', function (event) {
     if (!$(event.target).closest('#dropdown').length) {
